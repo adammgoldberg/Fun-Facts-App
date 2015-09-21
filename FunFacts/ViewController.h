8c8960f418ae8bd63ae93402eb54409b6b24b95c
Adam Goldberg <adammgoldberg@hotmail.com>
2015-09-21 14:49:44 -0400

    Initial Commit

---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by Adam Goldberg on 2015-09-21.
+//  Copyright (c) 2015 Adam Goldberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
